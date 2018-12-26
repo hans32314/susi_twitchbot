f76591b1586f5cc02f7d4dd48885f4d0388e5cc2
hans32314 <42351375+hans32314@users.noreply.github.com>
2018-12-26 15:13:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ client.on('chat', function(channel, userstate, message, self){
 				qs:
 				{
 					timezoneOffset: '-300',
-					q: u[1].substring((process.env.USERNAME).length + 1, u[1].length)
+					q: u[0].length + 1, u[1].length)
 				}
 			};
 
